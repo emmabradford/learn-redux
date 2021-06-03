688aae4f03d3a699f330d1341f27a6c39a5d7c2c
EmmaAbe <emma.abe.bradford@gmail.com>
2021-06-03 10:59:42 -0500

    added buttons and abulitu to inc and dec the state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import {useSelector} from 'react-redux';
+import {useSelector, useDispatch} from 'react-redux';
+import {increment} from './actions';
+import {decrement} from './actions';
 
 function App() {
   const counter = useSelector(state => state.counter);
   const isLogged = useSelector(state => state.isLogged);
-
+  const dispatch = useDispatch();
   return (
     <div className="App">
      <h1>Counter: {counter}</h1>
-     <button> + </button>
-     <button> - </button>
+     <button onClick={() => dispatch(increment(5))}> + </button>
+     <button onClick={() => dispatch(decrement())}> - </button>
      {isLogged ? 
         <h3> Info I should only see if logged in</h3> :
         <h3> You are not logged in</h3>
